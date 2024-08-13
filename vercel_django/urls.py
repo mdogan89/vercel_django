69fc261614d4f9f761e4773df2f414b4d79671c0
Mustafa Dogan <mdogan89@hotmail.com>
2024-08-13 04:28:21 +0300

    added vercel.json

--- vercel_django/urls.py
+++ vercel_django/urls.py
@@ -18,5 +18,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from vercel_test import urls
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [path("admin/", admin.site.urls), path("", include(urls))]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
